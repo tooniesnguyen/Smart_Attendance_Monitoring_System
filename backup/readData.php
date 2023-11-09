4a4c87642debf8a630e4c1685a20027a16e2e72e
sudo ./ubuntu-initial.sh <nnnhan1882002@gmail.com>
2023-11-09 20:18:36 +0700

    update web

---
+++ backup/readData.php
@@ -0,0 +1,20 @@
+<?php
+[AcceptVerbs(HttpVerbs.Post | HttpVerbs.Get)];
+header('Content-Type: application/json');
+
+// dang nhap vao database
+include("config.php");
+
+// Doc gia tri RGB tu database
+$sql = "select * from login";
+$result = mysqli_query($conn,$sql);
+
+$data = array();
+foreach ($result as $row){
+    $data[] = $row;
+}
+
+mysqli_close($conn);
+echo json_encode($data);
+
+?>

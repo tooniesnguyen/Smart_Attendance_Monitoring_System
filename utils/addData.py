2fef5d7f2f2b2305350caf130537cbd81c2516d8
sudo ./ubuntu-initial.sh <nnnhan1882002@gmail.com>
2023-11-11 10:06:54 +0700

    Update Frontend Lecture

--- utils/addData.py
+++ utils/addData.py
@@ -1,17 +1,20 @@
 import mysql.connector
 
-myconn = mysql.connector.connect(host = "localhost", user = "Nhan", passwd = "Nh@n", database = "IOT_Face")
-cur = myconn.cursor()
 
-rfid = "qweqwe123"
-checkin = "14:25"
 
-try:
-    cur.execute(f"update 20146_class set checkin = '{checkin}' where rfid = '{rfid}'")
-    myconn.commit()
-    print("Update successs")
+def addData(rfid: str, checkin: str):
+    myconn = mysql.connector.connect(host = "localhost", user = "Nhan", passwd = "Nh@n", database = "IOT_Face")
+    cur = myconn.cursor()
+    try:
+        cur.execute(f"update 20146_class set checkin = '{checkin}' where rfid = '{rfid}'")
+        myconn.commit()
+        print("Update successs")
 
-except:
-    myconn.rollback()
+    except:
+        myconn.rollback()
 
-myconn.close()
+    myconn.close()
+
+
+if __name__ == '__main__':
+    addData("123123asd", "14:28")
